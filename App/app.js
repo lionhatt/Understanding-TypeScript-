338eeedb1601801f123b22f12e90d2eb3e4ee54d
lionhatt <gavindxy@gmail.com>
2021-08-20 15:59:58 +1000

    union types'

--- App/app.js
+++ App/app.js
@@ -1,16 +1,13 @@
-// const person: {
-//     name: string;
-//     age: number
-// } = {
-var person = {
-    name: "Maximilian",
-    age: 30,
-    hobbies: ['Sports', 'Cooking']
-};
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
+var combineAges = combine(30, 26);
+console.log(combineAges);
+var combineNames = combine("Max", "Anna");
--- App/app.ts
+++ App/app.ts
@@ -1,40 +1,14 @@
-// const person: {
-//     name: string;
-//     age: number
-// } = {
-// const person:{
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string]
-// } = {
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role {ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR'};
-
-const person = {
-  name: "Maximilian",
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN
-};
-
-// person.role.push('admin');
-// person.role[1] = 10;
-
-// person.role = [0, 'admin', 'user'];
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-if (person.role == Role.AUTHOR) {
-    console.log("is author")
-}
+const combineAges = combine(30, 26);
+console.log(combineAges);
+
+const combineNames = combine("Max", "Anna");
