c18b243003a6b8da42fdf839fa93698987a873cc
lionhatt <gavindxy@gmail.com>
2021-08-20 15:36:25 +1000

    array types

--- App/app.js
+++ App/app.js
@@ -1,19 +1,16 @@
-// console.log("Your code goes here!")
-function add(n1, n2, showResult, phrase) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //     throw new Error('Incorrect input!');
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
+// const person: {
+//     name: string;
+//     age: number
+// } = {
+var person = {
+    name: "Maximilian",
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var numebr2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-var result = add(number1, numebr2, printResult, resultPhrase);
-console.log(result);
