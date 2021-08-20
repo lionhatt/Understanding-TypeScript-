b54db5a2799f442b64a0cbdf92752e76afcc174c
lionhatt <gavindxy@gmail.com>
2021-08-20 16:15:16 +1000

    type alias

--- App/app.ts
+++ App/app.ts
@@ -1,7 +1,10 @@
+type Combinable = number | string;
+type ConverstionDescriptor = 'as-number' | 'as-text'
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConverstionDescriptor
 ) {
   let result;
   if (
